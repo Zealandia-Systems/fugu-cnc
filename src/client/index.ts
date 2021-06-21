e309882fee726177f3b985f46c1896647d00c970
Alex Miller <codex.nz@gmail.com>
2021-06-22 10:40:03 +1200

    Format and lint existing code

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,8 @@
-import { IPort } from "../common/Port";
 import { ipcRenderer } from "electron";
+import { IPort } from "../common/Port";
 
-async() => {
-    const ports = await ipcRenderer.invoke('ports:list') as IPort[];
+async () => {
+  const ports = (await ipcRenderer.invoke("ports:list")) as IPort[];
 
-    console.log(JSON.stringify(ports));
-}
+  console.log(JSON.stringify(ports));
+};
